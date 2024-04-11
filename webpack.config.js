37f1b1e4ae4afbcf079aaf2c3d276ee47659c7d6
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-04-11 14:36:57 +0900

    init: configure eslint, prettier, typescript and webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const EslintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.ts"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src/index.html"),
+        filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+    // new CopyPlugin({
+    //     patterns: [
+    //       {
+    //         from: path.resolve(__dirname, "src/components/view/img"),
+    //         to: "img", // Updated to simplify the 'to' path
+    //       },
+    //     ],
+    // }),
+    new EslintPlugin({ extensions: ['ts'] }),
+  ],
+  resolve: {
+    alias: {
+      img: path.join(__dirname, "src", "components", "view", "img"),
+    },
+    extensions: ['.ts', '.js']
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png|svg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+      },
+    ],
+  },
+};
