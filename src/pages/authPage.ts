b5977313b9feee54f5028f37e083ec62164be13d
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-04-15 16:01:59 +0900

    feat: add styling for auth page

--- src/pages/authPage.ts
+++ src/pages/authPage.ts
@@ -5,34 +5,51 @@ export default class AuthPage {
 
     constructor() {
         this.form = new Tag('form', { class: 'login-form' });
-        this.addTitle('Login Page');
+        this.addTitle('Login');
         this.addInput('username', 'Username', false);
         this.addInput('password', 'Password', true);
         this.addSubmitButton('Login');
+        this.addAboutButton('About');
+        this.addCornerImage();
     }
 
     private addTitle(text: string): void {
-        const title = new Tag('h2');
+        const title = new Tag('h2', { class: 'login-form-header' });
         title.addText(text);
         this.form.addChild(title.render());
     }
 
     private addInput(name: string, placeholder: string, isPassword: boolean = false): void {
+        const inputContainer = new Tag('div', { class: 'input-container' });
+
         const inputType = isPassword ? 'password' : 'text';
         const inputAttributes = {
             id: name,
             name,
             type: inputType,
             placeholder,
+            class: 'input-field',
         };
         const input = new Tag('input', inputAttributes);
-        this.form.addChild(input.render());
+        inputContainer.addChild(input.render());
+        this.form.addChild(inputContainer.render());
     }
 
     private addSubmitButton(text: string): void {
-        const button = new Tag('button', { type: 'submit' });
+        const button = new Tag('button', { type: 'submit', class: 'login-form-submit', disabled: 'true' });
         button.addText(text);
         this.form.addChild(button.render());
+    }
+
+    private addAboutButton(text: string): void {
+        const button = new Tag('button', { class: 'login-form-about' });
+        button.addText(text);
+        this.form.addChild(button.render());
+    }
+
+    private addCornerImage(): void {
+        const cornerImage = new Tag('img', { src: 'https://freesvg.org/img/totoro.png', class: 'login-form-image' });
+        this.form.addChild(cornerImage.render());
     }
 
     render(): HTMLElement {
