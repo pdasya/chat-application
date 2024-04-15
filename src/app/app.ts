149181d1ad16613073cdf55b96da9680cf72a811
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-04-15 16:29:11 +0900

    feat: implement routeChange for pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,30 @@
 import AuthPage from '../pages/authPage';
+import { PageIds } from '../core/types/enums';
 
 export default class App {
     private static body: HTMLElement = document.body;
+    private static url: string | null = PageIds.Default;
 
     private static auth = new AuthPage();
+
+    static renderNewPage(idPage: string): void {
+        if (App.url) App.url = null;
+
+        if (idPage === PageIds.Auth) {
+            const page = new AuthPage();
+            App.url = idPage;
+            const content = page.render();
+            App.body.innerHTML = '';
+            App.body.appendChild(content);
+        }
+    }
+
+    static enableRouteChange(): void {
+        window.addEventListener('hashchange', () => {
+            const hash = window.location.hash.slice(1);
+            App.renderNewPage(hash);
+        });
+    }
 
     static generatePage(): void {
         const pageWrapper = document.createElement('div');
@@ -14,5 +35,8 @@ export default class App {
 
     static runApp(): void {
         App.generatePage();
+        App.enableRouteChange();
+        window.location.hash = PageIds.Auth;
+        App.renderNewPage(PageIds.Auth);
     }
 }
---
+++ src/core/types/enums.ts
@@ -0,0 +1,7 @@
+const enum PageIds {
+    Auth = 'auth',
+    Main = 'main',
+    Default = 'current-page',
+}
+// eslint-disable-next-line import/prefer-default-export
+export { PageIds };
