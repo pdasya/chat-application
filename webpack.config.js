c7a796756f9ab65ab3956a7bef933eec2fc3f5a9
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-04-22 14:37:27 +0900

    fix: fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path = require("path");
+const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
 const EslintPlugin = require('eslint-webpack-plugin');
 
-const baseConfig = {
+module.exports = {
   entry: path.resolve(__dirname, "src/index.ts"),
-  mode: 'development',
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: 'bundle.js',
@@ -18,14 +17,6 @@ const baseConfig = {
         favicon: 'src/assets/favicons/chat_favicon_32x32.png',
     }),
     new CleanWebpackPlugin(),
-    // new CopyPlugin({
-    //     patterns: [
-    //       {
-    //         from: path.resolve(__dirname, "src/components/view/img"),
-    //         to: "img", // Updated to simplify the 'to' path
-    //       },
-    //     ],
-    // }),
     new EslintPlugin({ extensions: ['ts'] }),
   ],
   resolve: {
@@ -34,10 +25,10 @@ const baseConfig = {
     },
     extensions: ['.ts', '.js']
   },
-  // devServer: {
-  //   open: true,
-  //   host: "localhost",
-  // },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
   module: {
     rules: [
       {
@@ -54,12 +45,4 @@ const baseConfig = {
       },
     ],
   },
-};
-
-module.exports = ({ mode }) => {
-  const isProductionMode = mode === 'prod';
-  // eslint-disable-next-line global-require
-  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
-
-  return merge(baseConfig, envConfig);
 };
