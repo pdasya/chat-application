b834b8645dc6c53162cc5d1cbf90738c1f2217a5
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-07-23 10:45:37 +0900

    fix: fix linter and package errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-const path = require("path");
+const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.ts"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src/index.html"),
-        filename: "index.html",
-        favicon: 'src/assets/favicons/chat_favicon_32x32.png',
-    }),
-    new CleanWebpackPlugin(),
-    new EslintPlugin({ extensions: ['ts'] }),
-  ],
-  resolve: {
-    alias: {
-      img: path.join(__dirname, "src", "components", "view", "img"),
+    entry: path.resolve(__dirname, 'src/index.ts'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
-    extensions: ['.ts', '.js']
-  },
-  devServer: {
-    open: true,
-    host: "localhost",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jpg|png|svg|jpeg|gif)$/,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.ts$/i,
-        use: "ts-loader",
-      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            favicon: 'src/assets/favicons/chat_favicon_32x32.png',
+        }),
+        new CleanWebpackPlugin(),
+        new EslintPlugin({ extensions: ['ts'] }),
     ],
-  },
+    resolve: {
+        alias: {
+            img: path.join(__dirname, 'src', 'components', 'view', 'img'),
+        },
+        extensions: ['.ts', '.js'],
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png|svg|jpeg|gif)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader',
+            },
+        ],
+    },
 };
